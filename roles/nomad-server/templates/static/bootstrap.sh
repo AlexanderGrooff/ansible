c9fea21fbc014aa32c9bec533ba553c5c7ca3341
Alexander Grooff <alexandergrooff@gmail.com>
2021-10-19 17:34:59 +0200

    feat: separate debian and arch

--- roles/nomad-server/templates/static/bootstrap.sh
+++ roles/nomad-server/templates/static/bootstrap.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/bash
 
 set -e
-set -x
 
 echo "Prepping env for running ansible"
 grep alex /etc/passwd || sudo useradd alex
 echo '%alex ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/0pw4alex
-sudo apt-get update
-sudo apt-get install -y python3-pip virtualenvwrapper
-. /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
+if [[ $(command -v apt-get) ]]; then
+    sudo apt-get update
+    sudo apt-get install -y python3-pip virtualenvwrapper
+elif [[ $(command -v pacman) ]]; then
+    git clone https://aur.archlinux.org/yay.git
+    pushd yay
+    makepkg -si --noconfirm
+    popd
+    rm -rf yay
+
+    yay -S --noconfirm python-pip python-virtualenv
+fi
 
 echo "Retrieving playbooks"
-git clone git@github.com/AlexanderGrooff/ansible.git
+if [ ! -d ansible ]; then
+    git clone https://github.com/AlexanderGrooff/ansible.git
+fi
 pushd ansible
 
 echo "Creating environment"
-mkvirtualenv -a . -p python3 $(basename $(pwd))
+if [ -d .venv ]; then
+    virtualenv .venv
+fi
+. .venv/bin/activate
 pip install -r requirements.txt
+ansible-galaxy collection install -r requirements-ansible-collections.yml
+if [ ! -f ansible-password.secret ]; then
+    echo "Dummy password so that ansible doesn't complain about missing password" > ansible-password.secret
+fi
 
 echo "Applying playbooks"
-ansible-playbook core.yml -v -i localhost,
+ansible-playbook core.yml -v -i localhost, --connection=local
 
 popd
