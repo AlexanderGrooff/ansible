253e908846a7430ed6789fd674ca9e0694b43e72
Alexander Grooff <alexandergrooff@gmail.com>
2021-10-19 14:42:22 +0200

    fix: install all files in static

--- roles/nomad-server/templates/static/bootstrap.sh
+++ roles/nomad-server/templates/static/bootstrap.sh
@@ -3,19 +3,22 @@
 set -e
 set -x
 
-test "$(whoami)" == 'root' || (echo "You're not root!" && exit 1)
-
 echo "Prepping env for running ansible"
 grep alex /etc/passwd || sudo useradd alex
-echo '%alex ALL=(ALL:ALL) NOPASSWD:ALL' | tee /etc/sudoers.d/0pw4alex
-apt-get update
-apt-get install -y python3-pip virtualenvwrapper git
+echo '%alex ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/0pw4alex
+sudo apt-get update
+sudo apt-get install -y python3-pip virtualenvwrapper
 . /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
 echo "Retrieving playbooks"
 git clone git@github.com/AlexanderGrooff/ansible.git
-cd ansible
+pushd ansible
 
 echo "Creating environment"
 mkvirtualenv -a . -p python3 $(basename $(pwd))
 pip install -r requirements.txt
+
+echo "Applying playbooks"
+ansible-playbook core.yml -v -i localhost,
+
+popd
