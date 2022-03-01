cf3ba445d5cd5098a73c9ec69078fd26166ce61e
Alexander Grooff <alexandergrooff@gmail.com>
2022-03-01 15:28:02 +0100

    feat: update to python 3.9

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
-        pkgs.python37
+        pkgs.python39
+        pkgs.python39Packages.wheel
     ];
 }
 
