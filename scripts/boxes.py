b8cb866642a00eaf46dc20b39f2d7adef1703b68
Alexander Grooff <alexandergrooff@gmail.com>
2022-03-21 17:22:49 +0100

    fix: run cmd local if localhost
    Also add logging

--- scripts/boxes.py
+++ scripts/boxes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import multiprocessing
+import socket
+import sys
 from argparse import ArgumentParser, Namespace
 from itertools import repeat
 from subprocess import check_output
@@ -15,27 +17,39 @@ from ansible.parsing.dataloader import DataLoader
 def get_all_hosts() -> List[str]:
     data_loader = DataLoader()
     inventory = InventoryManager(loader=data_loader, sources=["inventory.yml"])
-    return inventory.get_hosts()
+    return [str(h) for h in inventory.get_hosts()]
+
+
+def setup_logging(verbose=False):
+    logger.remove()
+    logger.add(sys.stdout, level="DEBUG" if verbose else "INFO")
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("hosts", help="SSH targets", nargs="*")
     parser.add_argument("-s", "--ssh", help="Command to run", default="w")
+    parser.add_argument("-v", "--verbose", help="Enable verbosity", action="store_true")
 
     return parser.parse_args()
 
 
-def run_command_on_host(target: str, command: str):
-    ssh_opts = [
-        "-T",  # Disable interactivity
-        "-oConnectTimeout=3",  # Timeout after X seconds
-        "-oBatchMode=yes",  # Don't ask for passwords
-    ]
-    ssh_cmd = f"ssh {' '.join(ssh_opts)} {target} {command}"
+def _compose_command(target: str, command: str) -> str:
+    if target != str(socket.gethostname()):
+        ssh_opts = [
+            "-T",  # Disable interactivity
+            "-oConnectTimeout=3",  # Timeout after X seconds
+            "-oBatchMode=yes",  # Don't ask for passwords
+        ]
+        command = f"ssh {' '.join(ssh_opts)} {target} {command}"
+    return command
+
+
+def run_command_on_host(target: str, command_on_target: str):
+    cmd = _compose_command(target=target, command=command_on_target)
     try:
-        logger.debug(f"Running on {target}: {command}")
-        output = check_output(ssh_cmd, shell=True)
+        logger.debug(f"Running on {target}: {cmd}")
+        output = check_output(cmd, shell=True)
         print(f"{target}: {output.decode('utf-8')}")
     except KeyboardInterrupt:
         return
@@ -50,5 +64,6 @@ def boxes(targets: List[str], command: str):
 
 if __name__ == "__main__":
     args = parse_args()
+    setup_logging(args.verbose)
     hosts = args.hosts or get_all_hosts()
     boxes(targets=hosts, command=args.ssh)
