ee811c4ad2fda71ce6311558a058618e57e0f97f
Alexander Grooff <alexandergrooff@gmail.com>
2022-03-29 20:50:13 +0200

    fix: remove nix, update alpha key

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [
-        pkgs.python39
-        pkgs.python39Packages.wheel
-    ];
-}
